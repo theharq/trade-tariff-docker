bc965174bbb890a56fcce4efffdba268b659a769
Hildebrando Rueda <harq.soft@gmail.com>
2016-02-12 13:02:08 -0500

    Remove shell files from signonotron

--- signonotron2/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export RAILS_ENV=docker
-service postfix start
-cd src
-bundle exec rake db:create db:migrate db:seed
-bundle exec rake docker:tariff_applications
-bundle exec rake users:create name="docker" email="docker@bitzesty.com" applications="trade-tariff-admin,trade-tariff-backend"
