978214608490507f7ca356fcb4fadfd7ce77a793
Hildebrando Rueda <harq.soft@gmail.com>
2016-02-12 10:20:17 -0500

    Update docker-compose file to build container for admin

--- trade-tariff-admin/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export RAILS_ENV=docker
-cd src
-bundle exec rake db:create db:migrate db:seed
--- trade-tariff-admin/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd src
-
-export RAILS_ENV=docker
-bundle install
-GDS_SSO_STRATEGY=real bundle exec unicorn -p 3046
